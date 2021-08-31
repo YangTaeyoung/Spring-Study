c000e2f2a5e042b8d56187544598d6bb297e3f6e
taeyoung <0130yang@gmail.com>
2021-08-31 21:35:24 +0900

    스프링 공부

---
+++ post/src/main/java/com/example/post/dto/PostRequestDto.java
@@ -0,0 +1,67 @@
+package com.example.post.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PostRequestDto {
+    private String account;
+    private String email;
+    private String address;
+    private String password;
+
+    @JsonProperty("phone_number")
+    private String phoneNumber; // json: phone_number
+    // 자바에서는 카멜케이스로 작성하는데 json에서는 스네이크 케이스로 작성하는 것을 권장하기 때문에
+    // 매핑이 되지 않는다.
+    // 이 때 @JsonProperty 어노테이션을 통해 매핑을 해줄 수 있다.
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "PostRequestDto{" +
+                "account='" + account + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
